2c2a576eba1a2b89243f768a12e666606d0620eb
Serafima <sunsam01@yahoo.com>
2022-07-05 22:11:46 +0300

    Поправила + кодстайл

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -17,9 +17,11 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-  if (!Array.isArray(array) || !array.length === 0) {
+  if (!Array.isArray(array) || array.length === 0) {
     throw new Error('empty array');
-  } else if (typeof fn !== 'function') {
+  }
+
+  if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
   }
 
@@ -49,13 +51,13 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (!Array.isArray(array) || !(array.length === 0)) {
+  if (!Array.isArray(array) || array.length === 0) {
     throw new Error('empty array');
   } else if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
   }
   for (const el of array) {
-    if (!fn(el)) {
+    if (fn(el)) {
       return true;
     }
   }
